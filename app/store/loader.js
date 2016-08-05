2164acd16d15ba4e1a4481e1237ec30e2591ce70
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-08-05 14:15:04 -0500

    fix store file load paths
    * now works in all browsers and on all devices

--- app/store/loader.js
+++ app/store/loader.js
@@ -1,8 +1,19 @@
 (function(context) {
-  context.importScripts('/build/js/es6-shim.min.js',
-                        '/build/js/lovefield.min.js',
-                        '/build/js/cache_and_sync_love.min.js',
-                        '/build/js/config.js',
-                        '/build/js/cache.js',
-                        '/build/js/store-worker.js');
+  var directory = '';
+
+  if (isApple()) {
+    directory = './build/js/';
+  }
+
+  context.importScripts(directory + 'es6-shim.min.js',
+                        directory + 'lovefield.min.js',
+                        directory + 'cache_and_sync_love.min.js',
+                        directory + 'config.js',
+                        directory + 'cache.js',
+                        directory + 'store-worker.js');
+
+  function isApple() {
+    return context.navigator.vendor &&
+           context.navigator.vendor.indexOf('Apple') > -1;
+  }
 })(this);
